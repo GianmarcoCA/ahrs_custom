6ab220aa2c1f5cd933d0fb4f0934dba2951d25f6
Juan Alva <juan_14-9@hotmail.com>
2025-11-27 11:28:55 -0800

    Test 3 types of euler data

--- test/python/prueba.py
+++ test/python/prueba.py
@@ -2,20 +2,26 @@ import subprocess
 import os
 import csv
 import time
+import argparse
 
 # usar doxygen
 
 print("Debug start...")
 
-BINARY_PATH = "./build/examples/c/7_series/ahrs/7_series_ahrs_example_c"
+parser = argparse.ArgumentParser(description="Capture logs from a stdout binary and save in .csv")
+parser.add_argument("-b", "--bin", default="./build/examples/c/7_series/ahrs/7_series_ahrs_example_c", help="Binary file path to execute")
+parser.add_argument("-o", "--out", required=True, help="Output file name to save in test/captures/")
+args = parser.parse_args()
+
+BINARY_FILE_PATH = args.bin#os.path.join("", args.bin)
 THIS_FILE_ABS_PATH = os.path.dirname(os.path.abspath(__file__))
 TEST_SCRIPT_PATH_REL_TO_PROJECT = "/test/python"
 PROJECT_PATH = THIS_FILE_ABS_PATH.replace(TEST_SCRIPT_PATH_REL_TO_PROJECT, "")
-BINARY_ABS_PATH = os.path.abspath(os.path.join(PROJECT_PATH, BINARY_PATH))
+BINARY_ABS_PATH = os.path.abspath(os.path.join(PROJECT_PATH, BINARY_FILE_PATH))
 
-CSV_OUTPUT_FILE = os.path.abspath(os.path.join(PROJECT_PATH, "test/captures/logs.csv"))
+CSV_OUTPUT_FILE = os.path.abspath(os.path.join(PROJECT_PATH, "test/captures/", args.out))#"test/captures/logs.csv"))
 
-
+print("BINARY_FILE_PATH:", BINARY_FILE_PATH)
 print("THIS_FILE_ABS_PATH:", THIS_FILE_ABS_PATH)
 print("PROJECT_PATH: ",PROJECT_PATH)
 print("BINARY_ABS_PATH:", BINARY_ABS_PATH)
@@ -26,10 +32,10 @@ SUDO = "sudo"
 
 with open(CSV_OUTPUT_FILE, "w", newline="") as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=';')
-    spamwriter.writerow(["timestamp", "log"]) # encabezado
+    spamwriter.writerow(["timestamp", "log", "tow", "roll", "pitch", "yaw"]) # encabezado
     
     process = subprocess.Popen(
-            [SUDO, BINARY_PATH],
+            [SUDO, BINARY_ABS_PATH],
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
             text=True,
@@ -39,8 +45,30 @@ with open(CSV_OUTPUT_FILE, "w", newline="") as csvfile:
     for line in process.stdout:
         line = line.strip()
         print(line)
-        
-        spamwriter.writerow([time.time(), line])
+ 
+        if "TOW" not in line:
+            spamwriter.writerow([time.time(), line, None, None, None, None])
+            csvfile.flush()
+            continue
+
+        try:
+            part_tow = line.split("TOW =")[1].split()[0]
+            tow     = float(part_tow)                
+            
+            inside = line.split("[")[1].split("]")[0]
+            parts = inside.split(",")
+
+            if len(parts) == 3:
+                roll    = float(parts[0])
+                pitch   = float(parts[1])
+                yaw     = float(parts[2])
+                
+            else:
+                roll = pitch = yaw = None
+        except:
+            tow = roll = pitch = yaw = None
+ 
+        spamwriter.writerow([time.time(), line, tow, roll, pitch, yaw])
         csvfile.flush()   # guardar en disco inmediatamente
         
 process.wait()
